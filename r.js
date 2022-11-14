f9dac6fab3a1bb6dbf0acdc7e41079481a637145
Musa Sayar <musasayar67@gmail.com>
2022-11-14 11:44:31 +0300

    Initial commit

---
+++ r.js
@@ -0,0 +1,184 @@
+
+
+function UI(){
+   this.btnLogin= document.querySelector(".btn1"),
+   this.createAcunt= document.querySelector("#createAccount"),
+   this.forgetPassword = document.querySelector("#forgetPassword"),
+   this.confirm= document.querySelector("#confirm"),
+   this.loginEnter= document.querySelector("#loginEnter")
+    //    this.password= document.querySelector("#signPassword")
+    //    this.toggle= document.querySelector("#toggle")
+//    this.confirmPassword = document.querySelector("#confirmPassword").value.length
+  
+   this.showError = document.querySelector(".f-input")
+   this.login1=document.querySelector("#login1")
+}
+ 
+
+UI.prototype.setMessage = function(formElement, type, message){
+    const messageElement= formElement.querySelector(".f-message");
+    
+
+    messageElement.textContent= message;
+    messageElement.classList.remove("f-message-success", "f-message-error");
+    messageElement.classList.add(`f-message-${type}`)
+
+}
+
+
+
+UI.prototype.showInputError=function(showElement,message) {
+    showElement.classList.add("f-input-error");
+   
+    showElement.parentElement.querySelector("#f-input-error-message").textContent=message;
+}
+
+UI.prototype.deleteInputError=function(showElement){
+    showElement.classList.remove("f-input-error");
+    showElement.parentElement.querySelector("#f-input-error-message").textContent="";
+}
+
+const ui = new UI()
+
+     document.addEventListener("DOMContentLoaded", ()=>{
+        const loginForm = document.querySelector("#loginArea")
+        const createNewAccount = document.querySelector("#accountArea")
+
+        ui.createAcunt.addEventListener("click", e =>{
+            e.preventDefault()
+            loginForm.classList.add("f-none");
+            createNewAccount.classList.remove("f-none")
+        });
+
+
+        ui.loginEnter.addEventListener('click', e =>{
+            e.preventDefault();
+            loginForm.classList.remove("f-none");
+            createNewAccount.classList.add("f-none")
+        })
+
+
+        loginForm.addEventListener("submit", e =>{
+            e.preventDefault();
+
+            ui.setMessage(loginForm,"error", "geçersiz şifre/ kullanıcı adı")
+        })
+     
+
+
+
+    document.querySelectorAll(".f-input").forEach(showElement=>{
+        showElement.addEventListener("blur", e=>{
+          
+
+            var regex = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+.)+([.])+[a-zA-Z0-9.-]{2,4}$/;
+                if(e.target.id==="login1"  && regex.test(e.target.value)==false){
+                    ui.showInputError(showElement,"Yanlış e-mail adresi girdniz")
+                }
+
+                if(e.target.id==="login1" && e.target.value === ""){
+                    ui.showInputError(showElement,"e-mail adresi girmediniz")
+                }
+                
+                // if(e.target.id==="login1" && e.target.value.length > 0 && e.target.value.length<4){
+                //     ui.showInputError(showElement,"kullanıcı adresi en 5 larakterli olmalı")
+                // }
+
+            if(e.target.id==="password1" && e.target.value.length<7 && e.target.value.length<12){
+                ui.showInputError(showElement,"Yanlış şifre girdiniz")
+            }
+
+
+
+
+            /*create account area */ 
+
+
+            if(e.target.id==="firstName"  && e.target.value.length < 2){
+                ui.showInputError(showElement,"Kullaıcı adı uzunluğu en az 3karakter olmalı")
+            }
+            
+
+            if(e.target.id==="signUsername"  && e.target.value.length>0 && e.target.value.length<6){
+                ui.showInputError(showElement,"Kullanıcı adı en az 6 karakterli olmalı")
+            }
+
+
+
+            if(e.target.id==="signEmail"  && regex.test(e.target.value)==false){
+                ui.showInputError(showElement,"email adresini  yanlış girdiniz")
+            }
+
+            
+
+            if(e.target.id==="signPassword" && e.target.value.length < 6 && e.target.value.length<10){
+                ui.showInputError( showElement," Şifreniz en az 6 karakter en fazla 12 karakter olamalı")
+            }
+
+        
+           const password= document.querySelector("#signPassword").value.length;
+           const confirmPassword = document.querySelector("#confirmPassword").value.length
+        
+
+            if(e.target.id==="confirmPassword" && password !== confirmPassword ){
+                ui.showInputError(showElement,"Paralolar benzer değil")
+            }
+
+            if(e.target.id==="agee" && e.target.value<18){
+                ui.showInputError(showElement,"18 yaşından küçüksünüz")
+            }
+
+          
+
+        })
+        showElement.addEventListener("input", e => {
+            ui.deleteInputError(showElement);
+
+
+        })
+    })
+   
+
+  
+
+   
+        
+  
+     })
+
+   
+
+
+function showPassword(){
+    const password = document.querySelector("#signPassword")
+    const eye = document.querySelector(".hide1")
+    const eyeSlash = document.querySelector(".hide2")
+
+
+    if(password.type==="password"){
+
+        password.type = "text";
+        eye.style.display="block";
+        eyeSlash.style.display= "none"
+    }else{
+        password.type = "password";
+        eye.style.display="none";
+        eyeSlash.style.display= "block"
+    }
+}
+
+function confirmPassword(){
+    const confirmPassword = document.querySelector("#confirmPassword")
+    const eye1 = document.querySelector(".hide3")
+    const eyeSlash2 = document.querySelector(".hide4")
+
+    if(confirmPassword.type==="password"){
+        confirmPassword.type="text"
+        eye1.style.display="block"
+        eyeSlash2.style.display="none"
+    }else{
+        confirmPassword.type="password"
+        eye1.style.display="none"
+        eyeSlash2.style.display="block"
+    }
+}
